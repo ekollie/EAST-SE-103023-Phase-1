712689c9ccb1e0e2e811b38b0d4e96955470ba69
Elijah <ekollie324@gmail.com>
2023-11-02 11:28:28 -0400

    commit

--- src/data.js
+++ src/data.js
@@ -1,3 +1,49 @@
+//✅ 1. fork and sync git
+
+//✅ 2. Review data.js and order of script tags in HTML <head> 
+
+function formatPrice(price) {
+  return '$' + Number.parseFloat(price).toFixed(2);
+}
+
+//✅ 3. explore the debugger
+
+//✅ 4. update the store name with "easley's technical books"
+
+function renderHeader(){
+  let storeName = bookStore.name
+  let storeNameNode = document.querySelector('#store-name')
+  storeNameNode.textContent = storeName
+}
+
+renderHeader();
+
+//✅ 5. create a function for render footer
+function renderFooter(){
+  let storeName = bookStore.name
+  let storeNumber = bookStore.number
+  let storeAddress = bookStore.address
+
+  let storeNameNode = document.querySelector('#store')
+  let storeNumberNode = document.querySelector('#number')
+  let storeAddressNode = document.querySelector('#address')
+}
+renderFooter()
+
+//✅ 6. create a function called renderBook(book)
+
+/***
+function: create HTML structure for single book and append to page
+input: a book object
+output: none
+***/
+
+//✅ 7. iterate over all the books in data and show book on page
+
+
+const titles = bookStore.inventory.map((el) => {el.title})
+console.log(titles);
+
 const bookStore = {
   location: "Seattle",
   address:'333 st ne Seattle wa 99999',
